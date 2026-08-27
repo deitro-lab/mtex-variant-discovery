6a1a83d3d7aad267ffdc9d592f8afb851da0ac9a
deitro-lab <deitro.research@gmail.com>
2026-08-27 10:14:09 +0800

    feat: add prompts per step Add text prompts for skipping steps win workflow Add implementation of ref indexing

--- var_discovery.py
+++ var_discovery.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
-import time, os
+import time
 from datetime import timedelta
 import copy
 import src.utilities as prlutil
 import src.preprocessing as prlprep
+import src.mapping as prlmap
 
 def main():
   time_start = time.perf_counter()
 
-  config_dir = input("Enter path for config file: ")
+  # config_dir = input("Enter path for config file: ")
+  config_dir = './var.local.config'
   options = prlutil.parse_config(config_dir)
   dir_list = options['DIRECTORIES']
 
@@ -23,8 +25,21 @@ def main():
 
   prlutil.init_project(dir_list.values())
 
-  fastp_cmd = prlprep.process_folder(dir_list['in_dir'], options['PARAM:INPUT'], {**dir_list, 'args': copy.copy(options['OPTIONS:FASTP'])})
-  prlutil.run_parallel(fastp_cmd, 4)
+  # Step 1: preprocessing
+  has_prep = ''
+  while has_prep.lower() not in ('y', 'yes', 'n', 'no'):
+    has_prep = input("Perform preprocessing step? (Y/N): ")
+  if has_prep == 'y' or has_prep == 'yes':
+    fastp_cmd = prlprep.process_folder(dir_list['in_dir'], options['PARAM:INPUT'], {**dir_list, 'args': copy.copy(options['OPTIONS:FASTP'])})
+    prlutil.run_parallel(fastp_cmd, 6)
+
+  # Step 2.1: Reference indexing
+  has_idx = ''
+  while has_idx.lower() not in ('y', 'yes', 'n', 'no'):
+    has_idx = input("Perform reference indexing? (Y/N): ")
+  if has_idx == 'y' or has_idx == 'yes':
+    idx_cmd = prlmap.index_ref(dir_list['ref_dir'])
+    prlutil.run_parallel(idx_cmd)
 
   time_span = timedelta(seconds=time.perf_counter()-time_start)
   print('Time used: ', time_span)
