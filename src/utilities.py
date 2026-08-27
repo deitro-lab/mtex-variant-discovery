c3ff4f7c9cc288d13b5e021059a0b12705769328
deitro-lab <deitro.research@gmail.com>
2026-08-27 10:10:27 +0800

    feat: add subprocess error handling Explicitly collect futures output Add error handling for subprocess

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,6 @@
 import os
 import configparser
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, wait, ALL_COMPLETED
 import subprocess
 
 def parse_config(config_path, config_defaults = None):
@@ -26,17 +26,24 @@ def init_project(folders):
 
 def run_command(cmd):
   print("Running command: " + cmd)
-  run_result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+  try:
+    run_result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+  except Exception as err:
+    print(f"Execution failed: {err}")
+    return err
+
   return run_result.stdout
 
 def run_parallel(cmd_queue, procs=None):
   if procs is None:
     procs = max(1, os.cpu_count() // 4)
 
+  subprocess.run('set -euo pipefail', shell=True)
   with ProcessPoolExecutor(max_workers=procs) as executor:
     try:
       run_results = executor.map(run_command, cmd_queue)
+      wait(run_results, return_when=ALL_COMPLETED)
     except Exception as err:
       print(f"Error occurred: {err}")
-
+  
   return run_results
