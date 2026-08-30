4f0e2a6ec6c921afe1f8b2ff3efb4bc2c3db0886
deitro-lab <deitro.research@gmail.com>
2026-08-30 08:38:16 +0800

    feat: add error reporting for run_command Provide basis logging for shell command errors

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,6 @@
 import os
 import configparser
-from concurrent.futures import ProcessPoolExecutor, wait, ALL_COMPLETED
+from concurrent.futures import ProcessPoolExecutor
 import subprocess
 
 def parse_config(config_path, config_defaults = None):
@@ -32,18 +32,22 @@ def run_command(cmd):
     print(f"Execution failed: {err}")
     return err
 
+  if run_result.stderr != '':
+    print(run_result.stderr)
+
   return run_result.stdout
 
 def run_parallel(cmd_queue, procs=None):
   if procs is None:
     procs = max(1, os.cpu_count() // 4)
 
-  subprocess.run('set -euo pipefail', shell=True)
   with ProcessPoolExecutor(max_workers=procs) as executor:
+    process_out = []
     try:
-      run_results = executor.map(run_command, cmd_queue)
-      wait(run_results, return_when=ALL_COMPLETED)
+      futures = executor.map(run_command, cmd_queue)
+      for res in futures:
+        process_out.append(res)
     except Exception as err:
       print(f"Error occurred: {err}")
   
-  return run_results
+  return process_out
